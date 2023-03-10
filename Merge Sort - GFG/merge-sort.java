76aa4ef88aa9ad8e801fbc0e4250166687d425bb
Priyansh Shah <shahpriyansh598@gmail.com>
2023-03-10 09:20:44 +0530

    Added solution - LeetHub

---
+++ Merge Sort - GFG/merge-sort.java
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Merge_Sort
+{
+    //method to print the elements of the array
+	static void printArray(int arr[])
+    {
+        StringBuffer sb=new StringBuffer("");
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            sb.append(arr[i]+" ");
+        System.out.println(sb.toString());
+    }
+
+    
+
+	public static void main(String args[])
+	{
+	    //taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking testcases
+		int T = sc.nextInt();
+		while(T>0)
+		{
+		    //taking elements count
+			int n = sc.nextInt();
+			
+			//creating an object of class Merge_Sort
+			Merge_Sort ms = new Merge_Sort();
+			
+			//creating an array of size n
+			int arr[] = new int[n];
+			
+			//adding elements to the array
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+            
+			Solution g = new Solution();
+			
+			//calling the method mergeSort
+			g.mergeSort(arr,0,arr.length-1);
+
+            //calling the method printArray
+			ms.printArray(arr);
+		T--;
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution
+{   //We use recursion for dividing first and merging after that.
+    //It uses divide & conquer with recursion and backtracking.
+    /*
+        Time Complexity:
+        Best case: O(nlogn)
+        Worst case: O(nlogn)
+        Avg. case:  O(nlogn)
+    */
+    void mergeSort(int arr[], int low, int high)
+    {   
+        //base case
+        if(low>=high)
+            return;
+        int mid=(low+high)/2;
+        mergeSort(arr,low,mid);
+        mergeSort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+    }
+    
+    void merge(int arr[], int low, int mid, int high)
+    {
+        List<Integer> temp= new ArrayList<Integer>();
+        
+        int left=low;
+        int right=mid+1;
+        
+        while(left<=mid && right<=high){
+            if(arr[left]<=arr[right]){
+                temp.add(arr[left]);
+                left++;
+            }
+            else{
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+        
+        //if right<=high is satisfied then few elements from the left we need to add which werent added
+        while(left<=mid){
+            temp.add(arr[left]);
+            left++;
+        }
+        
+        //if left<=mid is satisfied then few elements from the right we need to add which werent added
+        while(right<=high){
+            temp.add(arr[right]);
+            right++;
+        }
+        
+        for(int i=low; i<=high;i++){
+            arr[i]=temp.get(i-low);
+        }
+    }
+}
