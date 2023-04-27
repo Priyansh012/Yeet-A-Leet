3886643a0d339991915835b5b0a6d9c34d4afcaf
Priyansh Shah <shahpriyansh598@gmail.com>
2023-04-27 23:46:00 +0530

    Added solution - LeetHub

---
+++ Preorder Traversal - GFG/preorder-traversal.java
@@ -0,0 +1,167 @@
+//{ Driver Code Starts
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+                ArrayList<Integer> res = BinaryTree.preorder(root);
+                for (Integer num : res)
+                    System.out.print(num + " ");
+                System.out.println();
+                t--;
+            
+        }
+    }
+   
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+// class Node{
+//     int data;
+//     Node left,right;
+//     Node(int d){
+//         data=d;
+//         left=right=null;
+//     }
+// }
+
+class BinaryTree
+{
+    //Morris PreOrder Traversal TC: O(N), SC: O(1) 
+    //this is called as Binary Threaded Tree
+    static ArrayList<Integer> preorder(Node root)
+    {
+        ArrayList<Integer> preOrd = new ArrayList<Integer>();
+        Node cur = root;
+        
+        while(cur!=null){
+            
+            //case 1
+            if(cur.left==null){
+                preOrd.add(cur.data);
+                cur=cur.right;
+            }
+            
+            else{
+                Node prev= cur.left;
+                while(prev.right!=null && prev.right!=cur){
+                    prev=prev.right;
+                }
+                //case 2
+                if(prev.right==null){
+                    //thread created
+                    prev.right=cur;
+                    //normal preOrder traversal
+                    preOrd.add(cur.data);
+                    cur=cur.left;
+                }
+                
+                //case 3
+                else{
+                    //remove the thread
+                    prev.right=null;
+                    //normal preOrder traversal
+                    cur=cur.right;
+                }
+            }
+        }
+        
+        return preOrd;
+    }
+
+}
