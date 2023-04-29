4702d6eecc1da46e6f2fbe23d50d3a7d0e8d9696
Priyansh Shah <shahpriyansh598@gmail.com>
2023-04-29 15:19:22 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (76.90%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ //Inorder Traversal of a BST is always sorted(so TC: O(N) and no sorting required but SC: O(N))
+class Solution {
+    private int count=0;
+    private int result=0;
+
+    public int kthSmallest(TreeNode root, int k) {
+        inOrder(root, k);    
+        return result;
+    }
+
+    //Recusrive Inorder traversal approach
+    private void inOrder(TreeNode node, int k) {
+        if(node==null)
+            return;
+        
+        inOrder(node.left, k);
+        count++;
+        if (count == k) {
+            result = node.val;
+            return;
+        }
+        inOrder(node.right, k);
+
+    }
+
+
+}
+
