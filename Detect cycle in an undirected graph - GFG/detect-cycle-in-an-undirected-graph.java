e4990e8398781b425e6cc7836769f9ffe53bc379
Priyansh Shah <shahpriyansh598@gmail.com>
2023-05-29 08:27:19 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -31,25 +31,73 @@ class GFG {
 }
 // } Driver Code Ends
 
-class Pair{
-    int child;
-    int parent;
+//BFS
 
-    Pair(int _child, int _parent){
-        this.child=_child;
-        this.parent=_parent;
-    }
-}
+// class Pair{
+//     int child;
+//     int parent;
 
+//     Pair(int _child, int _parent){
+//         this.child=_child;
+//         this.parent=_parent;
+//     }
+// }
+
+// class Solution {
+//     // Function to detect cycle in an undirected graph.
+//     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+//         boolean visited [] = new boolean [V]; //by default all will be false and hence not visited in our context
+        
+//         for(int i=0 ; i<V; i++){
+//             //for multiple components in a graph
+//             if(!visited[i])
+//                 if(checkForCycle(i, V, adj, visited))
+//                     return true;
+//         }
+        
+//         return false;
+//     }
+    
+    
+//     private boolean checkForCycle(int src, int V, ArrayList<ArrayList<Integer>> adj, boolean visited []){
+//         visited[src]=true;
+//         Queue<Pair> q = new LinkedList<>();
+//         q.offer(new Pair(src, -1));
+        
+//         while(!q.isEmpty()){
+//             int node=q.peek().child;
+//             int parent=q.peek().parent;
+//             q.poll();
+            
+//             //traverse node's adj. list
+//             for(int adjacentNode : adj.get(node)){
+//                 if(visited[adjacentNode]==false){
+//                     visited[adjacentNode]=true;
+//                     q.offer(new Pair(adjacentNode, node));
+//                 }
+//                 //if the adj node of the node is not a parent and its already visited 
+//                 //then its a cycle in an undirected graph since some other node has already visited
+//                 else if(parent!=adjacentNode)
+//                     return true;
+//             }
+//         }
+        
+//         return false;
+
+//     }
+// }
+
+
+//DFS
 class Solution {
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
-        boolean visited [] = new boolean [V]; //by default all will be false and hence not visited in our context
         
-        for(int i=0 ; i<V; i++){
-            //for multiple components in a graph
-            if(!visited[i])
-                if(checkForCycle(i, V, adj, visited))
+        int visited [] = new int [V];
+        
+        for(int i=0;i<V;i++){
+            if(visited[i]==0)
+                if(dfs(i,-1, visited, adj)==true)
                     return true;
         }
         
@@ -57,30 +105,24 @@ class Solution {
     }
     
     
-    private boolean checkForCycle(int src, int V, ArrayList<ArrayList<Integer>> adj, boolean visited []){
-        visited[src]=true;
-        Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(src, -1));
+    private boolean dfs(int node, int parent, int visited [] , ArrayList<ArrayList<Integer>> adj){
         
-        while(!q.isEmpty()){
-            int node=q.peek().child;
-            int parent=q.peek().parent;
-            q.poll();
-            
-            //traverse node's adj. list
-            for(int adjacentNode : adj.get(node)){
-                if(visited[adjacentNode]==false){
-                    visited[adjacentNode]=true;
-                    q.offer(new Pair(adjacentNode, node));
+        visited[node]=1;
+        
+        for(int adjacentNode : adj.get(node)){
+                if(visited[adjacentNode]==0){
+                    if(dfs(adjacentNode, node, visited, adj)==true)
+                        return true;
                 }
+                
                 //if the adj node of the node is not a parent and its already visited 
                 //then its a cycle in an undirected graph since some other node has already visited
                 else if(parent!=adjacentNode)
-                    return true;
+                     return true;
             }
-        }
         
         return false;
-
     }
-}
+
+}    
+    
