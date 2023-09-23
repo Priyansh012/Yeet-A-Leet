ab132e3d179bf03c3a97f6661fb9c92ced295014
Priyansh Shah <shahpriyansh598@gmail.com>
2023-09-23 14:14:03 -0700

    Time: 0 ms (100.00%), Space: 41.2 MB (22.38%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -15,18 +15,40 @@
  */
 
 //Recursive Solution
- //Reverse post order can be used(Right, Left, Root)
+//  //Reverse post order can be used(Right, Left, Root)
+// class Solution {
+//     TreeNode prev= null;
+//     public void flatten(TreeNode root) {
+//         if(root==null)
+//             return;
+//         flatten(root.right);
+//         flatten(root.left);
+
+//         root.right=prev;
+//         root.left=null;
+
+//         prev=root;
+//     }
+// }
+
+
+//Iterative Solution using threaded Binary tree or Morris pre-order traversal
 class Solution {
-    TreeNode prev= null;
-    public void flatten(TreeNode root) {
-        if(root==null)
-            return;
-        flatten(root.right);
-        flatten(root.left);
+    public  void flatten(TreeNode root){
+        TreeNode cur= root;
 
-        root.right=prev;
-        root.left=null;
+        while(cur!=null){
+            if(cur.left!=null){
+                TreeNode prev=cur.left;
+                while(prev.right!=null){
+                    prev=prev.right;
+                }
+                prev.right=cur.right;
+                cur.right=cur.left;
+                cur.left=null;
 
-        prev=root;
+            }
+            cur=cur.right;
+        }
     }
 }
