8656f8513fe0fbd1d3c2d235568dd426e0f97ea5
Priyansh Shah <shahpriyansh598@gmail.com>
2023-12-10 10:13:12 -0500

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.java
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.java
@@ -31,24 +31,38 @@ class GFG {
 
 class Solve {
     int[] findTwoElement(int arr[], int n) {
-        // code here
-        int absValue;
-        int repeatingAndMissing []= new int [2];
-        
-        
-        for(int i=0;i<arr.length;i++){
-            absValue=Math.abs(arr[i]);
-            if(arr[absValue-1]>0)
-                arr[absValue-1] = -arr[absValue-1];
-            else
-                repeatingAndMissing[0]=absValue;
+        //X - repeating number
+        //Y - missing number
+        int result[]= new int[2];
+        long N=n;
+        //sum of all natural numbers
+        long sN= (N*(N+1))/2;
+        //sum of squares of all natural numbers
+        long s2N=(N*(N+1)*(2*N+1))/6;
+        //sum of elements in the array
+        long s=0;
+        //sum of squares of all elements in the array
+        long s2=0;
+        for(int i=0;i<n;i++){
+            s+=(long)arr[i];
+            s2+=(long)arr[i]*(long)arr[i];
         }
         
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>0)
-                repeatingAndMissing[1]= i+1;
-        }
+        //this will give X-Y
+        long val1=s-sN;
+        //this will give (X+Y)(X-Y)
+        long val2=s2-s2N;
+        //this will give X+Y
+        val2=val2/val1;
+        //2X
+        long X=(val2+val1)/2;
+        long Y=val2-X;
         
-        return repeatingAndMissing;
+        result[0]=(int)X;
+        result[1]=(int)Y;
+        
+        
+        return result;
+        
     }
 }
