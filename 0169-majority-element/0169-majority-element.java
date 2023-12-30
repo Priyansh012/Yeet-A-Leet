6ad98e080a8a7d8249897e054a835bc1175b8648
Priyansh Shah <shahpriyansh598@gmail.com>
2023-12-29 20:45:33 -0500

    Time: 2 ms (69.69%), Space: 50.3 MB (9.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,53 @@
 class Solution {
+    //Better than O(N^2)
+    //TC:O(NLogN)+O(N) and SC:O(N)
+    /*public int majorityElement(int[] nums) {
+        Map<Integer, Integer> elementCount = new HashMap<>();
+        int max=0;
+        int majorityElement=0;
+        for(int i=0;i<nums.length;i++){
+            elementCount.put(nums[i], elementCount.getOrDefault(nums[i],0)+1);
+        }
+
+        for(Map.Entry<Integer, Integer> element: elementCount.entrySet()){
+            if(max<element.getValue()){
+                max=element.getValue();
+                majorityElement=element.getKey();
+            }
+        }
+
+        return majorityElement;
+    }*/
+
+    //Optimized Soln with O(N) time and O(1) space
+    //Moore's Voting Algorithm
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> majorityMap = new HashMap<Integer, Integer>();
-        int result=0;
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            majorityMap.put(nums[i], majorityMap.getOrDefault(nums[i],0)+1);
-        }
+        int count=0;
+        int majorElement=0;
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                majorElement=nums[i];
+                count++;
+            }
+            else if(majorElement==nums[i])
+                count++;
+            else
+                count--;
+        }    
 
-         for (Map.Entry<Integer,Integer> mapElement : majorityMap.entrySet()) {
-             if(mapElement.getValue()>(n/2)){
-                result= mapElement.getKey();
-                break;
-             }
-        }
+        return majorElement;
 
-        return result;
+        //if we stop assuming that majority element exists all the time in the I/P
+        /*
+            int count2=0;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]==majorityElement)
+                    count2++;
+            }
+            if(count2>(nums.length/2))
+                return count2;
+            
+            return -1;
+        */
     }
 }
