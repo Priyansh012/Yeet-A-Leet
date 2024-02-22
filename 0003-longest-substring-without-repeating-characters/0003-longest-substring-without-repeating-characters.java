15c2e35605ae760f7f60b49e6205a8db3e6cbd6b
Priyansh Shah <shahpriyansh598@gmail.com>
2024-02-21 21:29:00 -0800

    Time: 5 ms (88.27%), Space: 44.3 MB (75.48%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,5 +1,7 @@
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
+
+    //Bruteforce
+    /*public int lengthOfLongestSubstring(String s) {
         HashMap<Character, Integer> characterToCount = new HashMap<Character, Integer>();
         int left;
         int max=0;
@@ -24,6 +26,58 @@ class Solution {
             if(max<count)
                 max=count;
             characterToCount.clear();
+        }
+
+        return max;
+    }*/
+
+    //Better
+   /* public int lengthOfLongestSubstring(String s) {
+        int left=0;
+        int right=0, count=0, max=0;
+        int n=s.length();
+        HashMap<Character, Integer> characterIndex = new HashMap<Character, Integer>();
+
+        if(n==1)
+            return 1;
+        while(left<=right && right<n){
+            if(!characterIndex.containsKey(s.charAt(right))){
+                characterIndex.put(s.charAt(right), right);
+                count+=1;
+            }
+
+            else{
+                left=characterIndex.get(s.charAt(right))+1;
+                count=right-left+1;
+                characterIndex.put(s.charAt(right), right);
+            }
+            max=Math.max(count, max);
+            right+=1;
+        }
+
+        return max;
+    }*/
+
+    //Optimal with slight tweak
+    public int lengthOfLongestSubstring(String s) {
+        int left=0;
+        int right=0, max=0;
+        int n=s.length();
+        HashMap<Character, Integer> characterIndex = new HashMap<Character, Integer>();
+
+        while(right<n){
+            if(characterIndex.containsKey(s.charAt(right))){
+                //slide left
+                left=Math.max(characterIndex.get(s.charAt(right))+1, left);
+            }
+
+            //put unique elements from the substring into the map
+            characterIndex.put(s.charAt(right), right);
+            //update max by comparing current length of substring with the old max
+            max=Math.max(max, right-left+1);
+
+            right+=1;
+
         }
 
         return max;
