6da411b4213ec863f36da166ee143b4a1ba0c00b
Priyansh Shah <shahpriyansh598@gmail.com>
2024-11-18 19:54:25 -0800

    Time: 20 ms (22.53%), Space: 46.4 MB (33.11%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -1,44 +1,74 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
+    // Two pass solution
+    // public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+    // List<Integer> result = new ArrayList<>();
+
+    // inOrder(root1, result);
+    // inOrder(root2, result);
+    // Collections.sort(result);
+    // return result;
+    // }
+    // private void inOrder(TreeNode root, List<Integer> result){
+    // Stack<TreeNode> st= new Stack<>();
+
+    // while(true){
+    // if(root!=null){
+    // st.push(root);
+    // root=root.left;
+    // }
+    // else{
+    // if(st.isEmpty())
+    // break;
+
+    // root=st.pop();
+    // result.add(root.val);
+    // root=root.right;
+    // }
+    // }
+
+    // }
+    // One pass solution
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
 
-        inOrder(root1, result);
-        inOrder(root2, result);
-        Collections.sort(result);
-        return result;
-    }
-    private void inOrder(TreeNode root, List<Integer> result){
-        Stack<TreeNode> st= new Stack<>();
+        List<Integer> res = new ArrayList<>();
 
-        while(true){
-            if(root!=null){
-                st.push(root);
-                root=root.left;
+        while (root1 != null || root2 != null || !st1.empty() || !st2.empty()) {
+            while (root1 != null) {
+                st1.push(root1);
+                root1 = root1.left;
             }
-            else{
-                if(st.isEmpty())
-                    break;
-                
-                root=st.pop();
-                result.add(root.val);
-                root=root.right;
+            while (root2 != null) {
+                st2.push(root2);
+                root2 = root2.left;
+            }
+            if (st2.empty() || (!st1.empty() && st1.peek().val <= st2.peek().val)) {
+                root1 = st1.pop();
+                res.add(root1.val);
+                root1 = root1.right;
+            } else {
+                root2 = st2.pop();
+                res.add(root2.val);
+                root2 = root2.right;
             }
         }
-        
+        return res;
     }
+
 }
