f1554c0ecbc530b2dbe830e5c3b7c4927a01a54c
Priyansh Shah <shahpriyansh598@gmail.com>
2024-12-25 07:54:24 -0800

    Time: 2 ms (99.11%), Space: 43.1 MB (61.82%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,5 +1,7 @@
 class Solution {
-    public boolean isPalindrome(String s) {
+    //TC: O(N)
+    //SC:O(N)
+    /*public boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
 
         for (char c : s.toCharArray()) {
@@ -30,5 +32,29 @@ class Solution {
             end-=1;
         }
         return true;
+    }*/
+    
+    public boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !alphaNum(s.charAt(l))) {
+                l++;
+            }
+            while (r > l && !alphaNum(s.charAt(r))) {
+                r--;
+            }
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            l++; r--;
+        }
+        return true;
+    }
+
+    public boolean alphaNum(char c) {
+        return (c >= 'A' && c <= 'Z' || 
+                c >= 'a' && c <= 'z' || 
+                c >= '0' && c <= '9');
     }
 }
