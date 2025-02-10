943f617a461d158654bb40b8fef4958ebd0bee44
Priyansh Shah <shahpriyansh598@gmail.com>
2025-02-10 15:07:10 -0800

    Time: 15 ms (6.17%), Space: 56.3 MB (5.46%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    //BFS
     public int goodNodes(TreeNode root) {
         Queue<Pair<TreeNode, Integer>> q= new LinkedList<>();
         q.add(new Pair(root, root.val));
@@ -53,4 +54,24 @@ class Solution {
 
         return count;
     }
+
+    //DFS
+    /*
+    int count = 0; // global variable to count
+
+    public int goodNodes(TreeNode root) {
+        helper(root, root.val);
+        return count; // returning the count value
+    }
+
+    private void helper(TreeNode root, int max) {
+        if (root != null) {
+            if (root.val >= max) { // if root.val > the max value of path from root of the tree to current node 
+                count++; //increment count
+            }
+            helper(root.left, Math.max(root.val, max));  // updating max value of current path and traversing left to the current node
+            helper(root.right, Math.max(root.val, max));  // updating max value of current path and traversing right to the current node
+        }
+    }
+    */
 }
