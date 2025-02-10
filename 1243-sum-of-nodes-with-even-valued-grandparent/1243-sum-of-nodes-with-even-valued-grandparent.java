cba4ad4d89adc220a42e14426d36bdac0f0e4370
Priyansh Shah <shahpriyansh598@gmail.com>
2025-02-10 12:30:15 -0800

    Time: 7 ms (21.1%), Space: 45.5 MB (98.65%) - LeetHub

--- 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.java
+++ 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.java
@@ -14,6 +14,7 @@
  * }
  */
 class Solution {
+    //BFS
     public int sumEvenGrandparent(TreeNode root) {
         if(root==null)
             return 0;
@@ -63,4 +64,21 @@ class Solution {
 
         return sum;
     }
+
+    //DFS
+    // public static int sum = 0;
+
+    // public int sumEvenGrandparent(TreeNode root) {
+    //     dfs(root, null, null);
+    //     return sum;
+    // }
+
+    // void dfs(TreeNode current, TreeNode parent, TreeNode grandParent) {
+    //     if (current == null) return; // base case 
+    //     if (grandParent != null && grandParent.val % 2 == 0) {
+    //         sum += current.val;
+    //     }
+    //     dfs(current.left, current, parent);// ( newChild, parent, GrandParent)
+    //     dfs(current.right, current, parent);
+    // }
 }
