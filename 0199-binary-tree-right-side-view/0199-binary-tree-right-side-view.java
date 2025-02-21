b455ec47b58a4757fe7d9c5ad9078dee8ac4c0c5
Priyansh Shah <shahpriyansh598@gmail.com>
2025-02-20 18:54:26 -0800

    Time: 1 ms (72.53%), Space: 41.8 MB (95.7%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,39 +15,29 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<Integer>();
-        rightView(root, result, 0);
+        if(root==null)
+            return new ArrayList();
+        
+        Queue<TreeNode> q = new LinkedList();
+        List<Integer> result = new ArrayList(); 
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int n=q.size();
+
+            for(int i=0;i<n;i++){
+                TreeNode node=q.poll();
+                if(i==n-1){
+                    result.add(node.val);
+                }
+
+                if(node.left!=null)
+                    q.add(node.left);
+                
+                if(node.right!=null)
+                    q.add(node.right);
+            }
+        }
         return result;
-    }
-
-    //For Right View, do reverse of pre-order traversal(Root right left)
-    public void rightView(TreeNode root, List<Integer> result, int level){
-        if(root==null)
-            return;
-        
-        if(level == result.size())
-            result.add(root.val);
-        
-        if(root.right!=null)
-            rightView(root.right, result, level+1);
-
-        if(root.left!=null)
-            rightView(root.left, result, level+1);
-    }
-
-        //For Left View, do pre-order traversal(Root left right)
-    public void leftView(TreeNode root, List<Integer> result, int level){
-        if(root==null)
-            return;
-        
-        if(level == result.size())
-            result.add(root.val);
-
-        if(root.left!=null)
-            leftView(root.left, result, level+1);
-
-        if(root.right!=null)
-            leftView(root.right, result, level+1);
-
     }
 }
