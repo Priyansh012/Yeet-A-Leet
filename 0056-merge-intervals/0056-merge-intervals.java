b680ea20df46f11c6c673618ed21919013883900
Priyansh Shah <shahpriyansh598@gmail.com>
2025-02-24 08:03:37 -0800

    Time: 7 ms (97.78%), Space: 46.4 MB (91.24%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,39 +1,21 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> a[0]==b[0]? Double.compare(a[1], b[1]): Double.compare(a[0], b[0]));
+        if (intervals.length <= 1) return intervals;
 
-        Map<Integer, Integer> result = new TreeMap<>();
-        int left=intervals[0][0];
-        int right=intervals[0][1];
-        int curLeft=0;
-        int curRight=0;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        result.put(left, right);
+        List<int[]> result = new ArrayList<>();
+        int[] current = intervals[0];
 
-        for(int i=1;i<intervals.length; i++){
-            curLeft=intervals[i][0];
-            curRight=intervals[i][1];
-
-            if(curLeft<=right){
-                right=Math.max(right, curRight);
-                result.put(left, right);
-            }
-            else{
-                left=curLeft;
-                right=curRight;
-                result.put(left,right);
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] <= current[1]) {
+                current[1] = Math.max(current[1], intervals[i][1]);
+            } else {
+                result.add(current);
+                current = intervals[i];
             }
         }
-
-        int ans [][] = new int [result.size()][2];
-       int index = 0;
-        for (Map.Entry<Integer, Integer> entry : result.entrySet()) {
-            ans[index][0] = entry.getKey();   
-            ans[index][1] = entry.getValue();
-            index++;
-        }
-
-        return ans;
-
+        result.add(current);
+        return result.toArray(new int[result.size()][]);
     }
 }
