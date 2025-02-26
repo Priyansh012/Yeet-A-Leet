54bc11acbe6c7fce55f5091f145918e023a40a44
Priyansh Shah <shahpriyansh598@gmail.com>
2025-02-25 21:05:43 -0800

    Time: 34 ms (20.86%), Space: 45.1 MB (91.52%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,49 +1,18 @@
 class Solution {
     public int firstUniqChar(String s) {
-       HashMap<Character, Integer> map = new HashMap<Character,Integer>();
-        int n= s.length();
-        int index=-1;
-        if(n==1)
-            return 0;
-        for(int i=0;i<n;i++){
-            if(map.containsKey(s.charAt(i))){
-                map.put(s.charAt(i),map.get(s.charAt(i))+1);
-            }
-            else
-                map.put(s.charAt(i),1);
+        Map<Character, Integer> frequencyMap= new HashMap();
+   
+        for(int i=0; i<s.length();i++){
+            char c=s.charAt(i);
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0)+1);
         }
-          for(int i=0;i<n;i++){
-             if(map.get(s.charAt(i))<2)
-             {
-                 index=i;
-                 break;
-             }
-        }        
-        return index;  
-    }
-}
 
-/*
-Optimal Solution
-class Solution {
-    public int firstUniqChar(String s) {
-        int[] count = new int[26];
-        int n = s.length();
-        // build char count bucket : <charIndex, count>
-        for (int i = 0; i < n; i++) {            
-            int index = s.charAt(i) - 'a';
-            count[index]++;
-        }
-        
-        // find the index
-        for (int i = 0; i < n; i++) {
-            int index = s.charAt(i) - 'a';
-            if (count[index] == 1) {
+        for(int i=0; i<s.length();i++){
+            if(frequencyMap.get(s.charAt(i))==1)
                 return i;
-            }
-                
         }
+
         return -1;
+         
     }
 }
-*/
