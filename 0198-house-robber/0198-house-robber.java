37dbb8723965f4e6448bc7c9eb341fbe7f73ee23
Priyansh Shah <shahpriyansh598@gmail.com>
2025-03-21 16:25:24 -0700

    Time: 0 ms (100%), Space: 40.6 MB (97.91%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -21,7 +21,7 @@ class Solution {
     }*/
 
     //Memoization appraoch : TC: O(N), SC: O(N)+O(N)
-    public int rob(int[] nums) {
+    /*public int rob(int[] nums) {
         int n=nums.length;
         int dp [] = new int [n];
         Arrays.fill(dp, -1);
@@ -45,6 +45,33 @@ class Solution {
         dp[index] = Math.max(pick, notPick);
 
         return dp[index];
-    }
+    }*/
 
+    //Tabulation appraoch : TC: O(N), SC: O(N)
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int dp [] = new int [n];
+        Arrays.fill(dp, -1);
+
+        // base cases
+        dp[0] = nums[0];
+        int negative = 0;
+        
+        for(int i=1; i<n; i++){
+            if(dp[i]!=-1)
+                return dp[i];
+            
+            int pick = nums[i];
+            if(i>1)
+                pick+=dp[i-2];
+            else
+                pick+=negative;
+            
+            int notPick = 0 + dp[i-1];
+    
+            dp[i] = Math.max(pick, notPick);
+        }
+
+        return dp[n-1];
+    }
 }
