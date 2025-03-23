9cc5e68c843d03669905a465f8a8198aa514dea9
Priyansh Shah <shahpriyansh598@gmail.com>
2025-03-23 00:04:43 -0700

    Time: 1 ms (20.22%), Space: 41.9 MB (50.08%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -24,7 +24,7 @@ class Solution {
     }*/
 
     //Memoization
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    /*public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         int dp[][] = new int [m][n];
@@ -54,5 +54,42 @@ class Solution {
 
         dp[i][j] = up+left;
         return dp[i][j];
+    }*/
+
+    //Tabulation
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int dp[][] = new int [m][n];
+        
+        for(int i=0;i<m;i++){
+            Arrays.fill(dp[i], -1);
+        }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                //obstacle
+                if(obstacleGrid[i][j]==1)
+                    dp[i][j]=0;
+                
+                //end reached so uniquePaths++
+                else if(i==0 && j==0)
+                    dp[i][j]=1;
+                
+                
+                else{
+                    int up=0, left=0;
+                    if(i>0)
+                        up=dp[i-1][j];
+                    
+                    if(j>0)
+                        left=dp[i][j-1];
+                    
+                    dp[i][j] = up+left;
+                } 
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 }
