2ec7b3d67b92499b08cb1f81043f638424fb31f5
Priyansh Shah <shahpriyansh598@gmail.com>
2025-03-23 08:56:40 -0700

    Time: 4 ms (62.89%), Space: 47.8 MB (44.51%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -21,7 +21,7 @@ class Solution {
     }*/
 
     //Memoization
-    public int minPathSum(int[][] grid) {
+    /*public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int dp [][] = new int[m][n];
@@ -50,5 +50,29 @@ class Solution {
         int minPrev = Math.min(up, left);
         dp[i][j] = grid[i][j] + minPrev;
         return dp[i][j];
+    }*/
+
+
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] dp = new int[m][n];
+
+        // Fill the dp table
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j]; // Start point
+                } else {
+                    int up = i > 0 ? dp[i - 1][j] : Integer.MAX_VALUE;
+                    int left = j > 0 ? dp[i][j - 1] : Integer.MAX_VALUE;
+                    dp[i][j] = grid[i][j] + Math.min(up, left);
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1]; // Bottom-right cell
     }
+
 }
